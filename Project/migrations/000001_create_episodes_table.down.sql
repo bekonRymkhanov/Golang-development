70f13c183cb56f52b7f25d8d43025621e20898a9
User <bekarys2004r@gmail.com>
2024-05-04 12:35:57 +0500

    a

--- Project/migrations/000001_create_episodes_table.down.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS episodes;
-DROP TABLE IF EXISTS characters;
-DROP TABLE IF EXISTS users;
--- Project/migrations/000002_add_episodes_check_constraints.down.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE episodes DROP CONSTRAINT IF EXISTS movies_runtime_check;
-ALTER TABLE episodes DROP CONSTRAINT IF EXISTS movies_year_check;
-ALTER TABLE episodes DROP CONSTRAINT IF EXISTS genres_length_check;
